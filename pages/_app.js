e83385107fe53b00599b7e8bd504aeb749772510
derpd <johnkuminga5@gmail.com>
2022-11-07 13:31:18 -0700

    Put GA Script in Head

--- pages/_app.js
+++ pages/_app.js
@@ -30,18 +30,17 @@ export default function MyApp({ Component, pageProps: { session, ...pageProps } 
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="viewport" content="width=1024"></meta>
-      </Head>
+        <Script src={"https://www.googletagmanager.com/gtag/js?id=" + config.public.googleAnalyticsId} strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-      <Script src={"https://www.googletagmanager.com/gtag/js?id=" + config.public.googleAnalyticsId} strategy="afterInteractive" />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${config.public.googleAnalyticsId}');
-        `}
+            gtag('config', '${config.public.googleAnalyticsId}');
+          `}
       </Script>
+      </Head>
 
       <Layout websiteTitle={config.public.websiteTitle} announcement={config.public.announcement} menuLinks={config.public.menuLinks} disclaimer={disclaimer} socialLinks={config.public.socialLinks} footerLinks={config.public.footerLinks} button={playButton}>
         <Component {...pageProps} />
