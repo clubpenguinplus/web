2f3a79563d9a1666bd617f1fb7c9f3284a844e0c
Octo <octo@clubpenguin.plus>
2022-11-07 23:20:46 +0000

    Revert "Update _app.js"
    This reverts commit 1a2784f62c36670111371ed5606fe9862b88c530.

--- pages/_app.js
+++ pages/_app.js
@@ -31,6 +31,17 @@ export default function MyApp({ Component, pageProps: { session, ...pageProps } 
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="viewport" content="width=1024"></meta>
       </Head>
+      
+      <Script src={"https://www.googletagmanager.com/gtag/js?id=" + config.public.googleAnalyticsId} strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${config.public.googleAnalyticsId}');
+          `}
+      </Script>
 
       <Layout websiteTitle={config.public.websiteTitle} announcement={config.public.announcement} menuLinks={config.public.menuLinks} disclaimer={disclaimer} socialLinks={config.public.socialLinks} footerLinks={config.public.footerLinks} button={playButton}>
         <Component {...pageProps} />
