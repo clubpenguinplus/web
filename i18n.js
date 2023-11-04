a4607a4a1d94cf4e1f26b56dfd01c28e5eba433c
Octo <octo@clubpenguin.plus>
2023-11-04 20:55:55 +0000

    update default language

--- i18n.js
+++ i18n.js
@@ -1,6 +1,6 @@
 module.exports = {
   locales: ["pt", "en", "es"], // Array with the languages that you want to use
-  defaultLocale: "pt", // Default language of your website
+  defaultLocale: "en", // Default language of your website
   pages: {
     "*": ["strings"], // Namespaces that you want to import per page (we stick to one namespace for all the application in this tutorial)
   },
